1ae23be05292b2a855651cde1cfb127397cc2f0d
Sam Wallace <samuelfrancis.wallace@uzh.ch>
2024-12-08 18:40:22 +0100

    Product of Array except self

--- Medium/Encode and Decode Strings.py
+++ Medium/Encode and Decode Strings.py
@@ -3,6 +3,7 @@ class Solution:
     def encode(self, strs: List[str]) -> str:
         res = ""
         for s in strs:
+            # add lend of string and delimator #
             res += str(len(s)) + "#" + s
         return res
 
@@ -15,9 +16,9 @@ class Solution:
             while s[j] != '#':
                 j += 1
             length = int(s[i:j])
-            i = j + 1
-            j = i + length
-            res.append(s[i:j])
+            i = j + 1 #Index to start of string
+            j = i + length # Get end index of string
+            res.append(s[i:j]) #append
             i = j
             
         return res
